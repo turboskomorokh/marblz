76affd18a5bb9858d3ecd2371023e62ef953ff82
turboskomorokh <turboskomorokh@gmail.com>
2025-07-05 21:03:54 +0300

    Changes

---
+++ game/ChunkBuilder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by turbo on 30.06.25.
+//
+
+#include "ChunkBuilder.h"
---
+++ game/ChunkBuilder.h
@@ -0,0 +1,15 @@
+//
+// Created by turbo on 30.06.25.
+//
+
+#ifndef MARBL_CHUNKBUILDER_H
+#define MARBL_CHUNKBUILDER_H
+
+
+class ChunkBuilder
+{
+
+};
+
+
+#endif //MARBL_CHUNKBUILDER_H
